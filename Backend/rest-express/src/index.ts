14a57d337efb8cf8c57edf7778cd0f4fa1decd0b
kemalmusth <kemalmusthafa80@gmail.com>
2024-11-18 16:24:18 +0700

    Updated 1.0

--- Backend/rest-express/src/index.ts
+++ Backend/rest-express/src/index.ts
@@ -1,9 +1,13 @@
+"use client"
+
 import express, {Request, Response, Application} from 'express'
 import { UserRouter } from './routers/user.router'
+import cors from "cors"
 
 const PORT: number = 8000
 
 const app: Application = express()
+app.use(cors())
 app.use(express.json())
 const userRouter = new UserRouter()
 
