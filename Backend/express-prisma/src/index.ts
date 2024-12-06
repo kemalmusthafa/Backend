b10c51f16b98908cb810a27b9e0d1a634353a993
kemalmusth <kemalmusthafa80@gmail.com>
2024-12-06 17:03:11 +0700

    Updated 06 December 2024

--- Backend/express-prisma/src/index.ts
+++ Backend/express-prisma/src/index.ts
@@ -5,6 +5,7 @@ import { BlogRouter } from "./routers/blog.router";
 import { AuthRouter } from "./routers/auth.routers";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { PostRouter } from "./routers/post.router";
 
 const PORT : number = 8000
 
@@ -25,10 +26,12 @@ app.use("/api/public", express.static(path.join(__dirname, "../public")))
 const userRouter = new UserRouter();
 const blogRouter = new BlogRouter();
 const authRouter = new AuthRouter();
+const postRouter = new PostRouter();
 
 app.use("/api/users", userRouter.getRouter());
 app.use("/api/blogs", blogRouter.getRouter());
 app.use("/api/auth", authRouter.getRouter());
+app.use("/api/posts", postRouter.getRouter());
 
 app.listen(PORT, () => {
     console.log(`Server running on => http://localhost:${PORT}/api`);
